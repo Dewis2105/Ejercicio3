f9e0d708255af986f03bfdb4667e6d00864bc93a
FChicmana <74842128@continental.edu.pe>
2024-09-10 14:44:45 -0500

    Refactorización de Actividad 3

---
+++ Actividad_3.py
@@ -0,0 +1,7 @@
+class Calculadora:
+    def calcular_multisuma(self, multiplicando1, multiplicando2, sumando):
+        resultado_multisuma = multiplicando1 * multiplicando2 + sumando
+        return resultado_multisuma
+
+    def elevar_al_cuadrado(self, numero):
+        return numero ** 2
